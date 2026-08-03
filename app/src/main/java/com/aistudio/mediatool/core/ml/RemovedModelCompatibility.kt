efc6df986df8e9367ba7c2a6c321847797c57330
tvhuy99-web <tvhuy99@gmail.com>
2026-08-03 08:19:35 +0700

    remove obsolete model compatibility shim

--- app/src/main/java/com/aistudio/mediatool/core/ml/RemovedModelCompatibility.kt
+++
@@ -1,5 +0,0 @@
-package com.aistudio.mediatool.core.ml
-
-@Deprecated("Mel-Band RoFormer đã bị loại bỏ")
-internal val StemModelRegistry.MEL_BAND_ROFORMER_ID: String
-    get() = "removed-mel-band-roformer"
